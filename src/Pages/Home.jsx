853e8965bc40a8e1e5b4ade89be808b18da9f8fd
Martz94 <130812797+Martz94@users.noreply.github.com>
2024-10-12 14:52:24 -0600

    ajuste titulo

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,9 +7,9 @@ const Home = () => {
   if (error) return <div className="text-red-500 text-center mt-48 h-screen">{error}</div>;
 
   return (
-    <div className="container mt-32 xl:mt-36">
-      <div className=' bg-zinc-800 py-5 mb-10 right-0 left-0'>
-        <h2 className="text-3xl md:text-5xl text-center md:text-start mx-auto md:ms-48 md:me-auto font-bold bg-gradient-to-r from-neutral-500 to-red-900 bg-clip-text text-transparent">
+    <div className="container mt-11 sm:mt-14">
+      <div className=' bg-zinc-800 py-6 mb-10 right-0 left-0'>
+        <h2 className="text-4xl md:text-5xl text-center md:text-start mx-auto w-auto md:max-w-6xl px-2 md:me-auto font-bold bg-gradient-to-r from-neutral-500 to-red-900 bg-clip-text text-transparent">
           Marvel Universe
         </h2>
       </div>
@@ -17,6 +17,10 @@ const Home = () => {
         <h3 className='text-2xl md:text-3xl font-semibold text-center my-8 underline bg-gradient-to-r from-red-400 to-red-700 bg-clip-text text-transparent'>Latest Marvel News</h3>
         {comics.slice(0, 10).map((comic) => (
           <div key={comic.id} className=" relative block sm:flex bg-white gap-3 shadow-lg rounded-lg overflow-hidden hover:shadow-red-500 border hover:border-red-500">
+            <img
+              src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
+              alt={comic.title}
+              className="w-full sm:w-60 h-56 sm:h-full object-cover cursor-pointer relative z-0 scale-110 transition-all duration-300 hover:scale-100" />
             <div className="p-4">
               <h3 className="text-xl font-semibold mb-2">{comic.title}</h3>
               <p className="text-gray-700 mb-4">
